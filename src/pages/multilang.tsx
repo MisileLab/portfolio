7b824cabd6ffaea078810b404434a4955729bbef
MisileLaboratory <misileminecord@gmail.com>
2023-07-09 15:26:36 +0900

    Update multilang.tsx

--- src/pages/multilang.tsx
+++ src/pages/multilang.tsx
@@ -1,6 +1,6 @@
 const MultiLang = () => {
     return (<div id="container" style="width: 100%; white-space: nowrap; display: inline-block;">
-    <h1>Programming Language that can i usually do is Python and Rust but other lang can do</h1><br></br>
+    <h1>Programming Language that i usually do is Python and Rust but other lang can do</h1><br></br>
     <h2 style="color: var(--ctp-mocha-yellow)">Like haskell and nim</h2>
     </div>)
 }
