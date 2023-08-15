fbea38e685e3ec51d1e3b9bf27c162838ae1dc3b
MisileLab <misileminecord@gmail.com>
2023-08-15 15:01:15 +0900

    add project

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -15,6 +15,7 @@ return (
             <li><a href="https://github.com/mcCmd-language/mcCmd">McCmd | Minecraft-style Interpreter</a></li>
             <li><a href="https://github.com/misilelab/h3/tree/main/projects/dsb/ch">Crypto Hashing | Encrypting text</a></li>
             <li><a href="https://github.com/misilelab/h3/tree/main/projects/sntapi">sntapi | Sunrint Meal Backend of </a><a href="https://www.instagram.com/today.sunrin.meal">instagram bot</a></li>
+            <li><a href="https://github.com/chat-ana/ana-chat">ana chat | Just simple chat using shuttle and Next.JS</a></li>
         </ul>
     </div>
     );
