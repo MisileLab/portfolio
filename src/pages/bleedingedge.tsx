b183eb96112dd04f3082e15f59aca48e7a767e2f
MisileLab <misileminecord@gmail.com>
2023-12-15 00:08:39 +0900

    a simple flake
    Signed-off-by: MisileLab <misileminecord@gmail.com>

--- src/pages/bleedingedge.tsx
+++ src/pages/bleedingedge.tsx
@@ -9,8 +9,7 @@ return (
         <div id="container" style="width: 100%; white-space: nowrap; display: inline-block;">
             <h1>Operating Systems</h1>
             <ul>
-                <li>Bleeding-edge Gentoo + Arch and EndeavourOS</li>
-                <li>Backup for bleeding-edge bugs</li>
+                <li>Bleeding-edge NixOS</li>
             </ul>
             {/* <b><h2><a href="https://status.misilelaboratory.xyz" style="color: var(--ctp-mocha-text); text-decoration: underline">My open-source hosting</a></h2></b> */}
         </div>
