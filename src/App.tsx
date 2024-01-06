58c60821646ce381e22702abe973859f3eaaf8d7
MisileLab <misileminecord@gmail.com>
2024-01-07 01:25:03 +0900

    asdf
    Signed-off-by: MisileLab <misileminecord@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,75 +1,124 @@
-import { createSignal } from 'solid-js';
-import BleedingEdge from './pages/bleedingedge';
-import OpenSource from './pages/open-source';
-import MultiLang from './pages/multilang';
-import styles from './App.module.css';
-import Projects from './pages/projects';
-import History from './pages/history';
-import About from './pages/about';
-import './theme.css';
-import './custom.css';
+import { HoverCard } from "@kobalte/core";
+import { Index, JSX } from "solid-js";
+import { AiFillGithub, AiOutlineMail } from 'solid-icons/ai';
+import { FaBrandsDiscord } from "solid-icons/fa";
+
+interface Project {
+  name: string;
+  description: string;
+  url: string;
+}
+
+interface Contribute {
+  name: string;
+  url: string;
+}
+
+interface Contact {
+  icon: JSX.Element,
+  content: string,
+  url: string
+}
+
+const projects: Project[] = [
+  {
+    "name": "Portfolio",
+    "description": "This page",
+    "url": "https://github.com/misilelab/portfolio"
+  },
+  {
+    "name": "Ignofier+",
+    "description": "Fast gitignore generator that made with Rust",
+    "url": "https://github.com/misilelab/ignofierplus"
+  },
+  {
+    "name": "Klojure",
+    "description": "Search koreanbots with multiple keywords",
+    "url": "https://github.com/lawnseol/klojure"
+  },
+  {
+    "name": "McCmd",
+    "description": "Minecraft-style Interpreter",
+    "url": "https://github.com/mcCmd-language/xmccmd"
+  },
+  {
+    "name": "sntapi",
+    "description": "Sunrint school meal api",
+    "url": "https://github.com/MisileLab/h3/tree/78c1d37ace0c612915e44e91e6cfec38d7842508/archives/sntapi"
+  },
+  {
+    "name": "calendar",
+    "description": "Simple calendar with tailwindcss, solidjs, tauri",
+    "url": "https://github.com/MisileLab/calendar"
+  }
+];
 
 const App = () => {
-  const [currentOption, setCurrentOption] = createSignal('option1');
-  const [currentOption2, setCurrentOption2] = createSignal('option2');
-
-  function handleChange(event: Event) {
-    const { value } = event.target as HTMLSelectElement;
-    setCurrentOption2(value);
-  }
-
-  function handleChangeForOption(event: Event) {
-    const { value } = event.target as HTMLSelectElement;
-    setCurrentOption(value);
-  }
-
-  function renderPage() {
-    switch (currentOption()) {
-      case 'opensource':
-        return <OpenSource />;
-      case 'multilang':
-        return <MultiLang />;
-      default:
-        return <BleedingEdge />;
+  const contacts: Contact[] = [
+    {
+      "content": "misileminecord@gmail.com",
+      "url": "mailto:misileminecord@gmail.com",
+      "icon": (<AiOutlineMail size={24} />)
+    },
+    {
+      "content": "misilelab",
+      "url": "https://github.com/misilelab",
+      "icon": (<AiFillGithub size={24} />)
+    },
+    {
+      "content": "misile",
+      "url": "https://discord.com",
+      "icon": (<FaBrandsDiscord size={24}/>)
     }
-  }
-
-  function renderPage2() {
-    switch (currentOption2()) {
-      case 'history':
-        return <History />
-      case 'about':
-        return <About />
-      default:
-        return <Projects />;
-    }
-  }
-
+  ];
   return (
-    <div class={styles.App}>
-      <div id="container">
-        <h1>
-          The{' '}
-          <select
-            name="everything"
-            id="everything"
-            style="margin-right: 5px;"
-            onChange={handleChangeForOption}
-          >
-            <option value="bleedingedge">Bleeding-Edge</option>
-            <option value="opensource">Open-source</option>
-            <option value="multilang">Multi-Language</option>
-          </select>
-          Laboratory - MisileLaboratory/
-          <select name="selectorhistory" id="selectorhistory" onChange={handleChange}>
-            <option value="projects">Projects</option>
-            <option value="about">About</option>
-            <option value="history">History</option>
-          </select>
-        </h1>
+    <div class="flex flex-col bg-ctp-base" style={"width: 100%;"}>
+      <div class="w-full h-screen flex justify-center items-center flex-col">
+        <div class="text-3xl text-ctp-subtext0 font-normal mb-2">Many <span class="text-ctp-red">fails</span> lead to one <span class="text-ctp-green">success</span></div>
+        <div class="text-4xl text-ctp-text font-bold">Misile</div>
+        <div class="flex flex-row mt-2 text-ctp-text space-x-1">
+          <Index each={contacts}>
+            {(i, _) => {
+              return (
+                <HoverCard.Root>
+                  <HoverCard.Trigger>
+                    {i().icon}
+                  </HoverCard.Trigger>
+                  <HoverCard.Content class="bg-ctp-surface0 hc">
+                    <HoverCard.Arrow />
+                    <p class="text-ctp-yellow">{i().content}</p>
+                  </HoverCard.Content>
+                </HoverCard.Root>
+              );
+            }}
+          </Index>
+        </div>
       </div>
-      {renderPage()}
-      {renderPage2()}
+      <div class="w-full flex flex-col items-center">
+        <div class="text-3xl text-ctp-text font-bold mt-4">
+          Projects
+        </div>
+        <div class="w-full flex flex-col justify-center items-center pl-20 pr-20">
+          <table class="mt-4 border-spacing-2 border-separate w-full border-solid border-ctp-overlay border-2">
+            <thead>
+              <tr class="text-xl text-ctp-subtext1 w-1/2">
+                <th>Title</th>
+                <th>Description</th>
+              </tr>
+            </thead>
+            <tbody class="text-2xl text-ctp-subtext0 text-center">
+              <Index each={projects}>
+                {(i, _) => {
+                  return (<tr>
+                    <td><a href={i().url} class="text-ctp-blue">{i().name}</a></td>
+                    <td>{i().description}</td>
+                  </tr>);
+                }}
+              </Index>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 };
--- src/pages/multilang.tsx
+++
@@ -1,8 +0,0 @@
-const MultiLang = () => {
-    return (<div id="container" style="width: 100%; white-space: nowrap; display: inline-block;">
-    <h1>Primary Language is Python, Rust, Kotlin</h1><br></br>
-    <h2 style="color: var(--ctp-mocha-yellow)">But I can do almost any lang</h2>
-    </div>)
-}
-
-export default MultiLang;
